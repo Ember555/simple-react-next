3404da483d7c9eeb3d98d289217ff7c8ed628bf6
Natthanan Chanthong <natthanan.cha@kbtg.tech>
2019-11-01 17:04:49 +0700

    update epic and reducer

--- src/reduxs/epic.js
+++ src/reduxs/epic.js
@@ -1,9 +1,7 @@
 import { combineEpics } from 'redux-observable'
 
-import { connectEpic } from './socket/epic'
 import { testPageEpic } from './testPage/epic'
 
 export default combineEpics(
-    connectEpic,
     testPageEpic,
 )
--- src/reduxs/reducer.js
+++ src/reduxs/reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
 
-import socket from './socket/reducer'
 import testPage from './testPage/reducer'
 
 export default combineReducers({
-    socket,
     testPage,
 })
